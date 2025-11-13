7d6f8e5175edbcbf4f37acb94e10c3be3cdc1240
gwjac <gwjackson53@gmail.com>
2025-11-13 13:32:11 -0700

    NOT version 1.000, still have lots to add, complete all the menus, and the reset and ... So really Version 0.3

--- src/H2FpEF.py
+++ src/H2FpEF.py
@@ -79,7 +79,7 @@ class Main_Frame(wx.Frame):
 
         self.create_menu()
         self.CreateStatusBar()
-        self.create_main_panel()
+        self.main_panel = self.create_main_panel()
 
     def create_menu(self):
         """
@@ -146,132 +146,134 @@ class Main_Frame(wx.Frame):
 
     # and now some sizers for the mainpanel plan is a 5 column x 10 row gridbagsizer
     def create_main_panel(self):
-        main_panel = wx.Panel(self)
-        mp_sizer = wx.GridBagSizer(10,10)
+        self.main_panel = wx.Panel(self)
+        self.mp_sizer = wx.GridBagSizer(10,10)
 
-        self.colblk = wx.StaticText(main_panel, label='  ')
-        mp_sizer.Add(self.colblk, pos=(0,0), flag=wx.ALL, border=5)
-        self.colclivar = wx.StaticText(main_panel, label='Clinical Variable')
-        mp_sizer.Add(self.colclivar, pos=(0,1), flag=wx.ALL, border=5)
-        self.colvalue = wx.StaticText(main_panel, label='Values')
-        mp_sizer.Add(self.colvalue, pos=(0,2), flag=wx.ALL, border=5)
-        self.colpoints = wx.StaticText(main_panel, label='Points\nScore')
-        mp_sizer.Add(self.colpoints, pos=(0,3), flag=wx.ALL, border=5)
-        self.colreg = wx.StaticText(main_panel, label='Regression\nequation')
-        mp_sizer.Add(self.colreg, pos=(0,4), flag=wx.ALL, border=5)
+        self.colblk = wx.StaticText(self.main_panel, label='  ')
+        self.mp_sizer.Add(self.colblk, pos=(0,0), flag=wx.ALL, border=5)
+        self.colclivar = wx.StaticText(self.main_panel, label='Clinical Variable')
+        self.mp_sizer.Add(self.colclivar, pos=(0,1), flag=wx.ALL, border=5)
+        self.colvalue = wx.StaticText(self.main_panel, label='Values')
+        self.mp_sizer.Add(self.colvalue, pos=(0,2), flag=wx.ALL, border=5)
+        self.colpoints = wx.StaticText(self.main_panel, label='Points\nScore')
+        self.mp_sizer.Add(self.colpoints, pos=(0,3), flag=wx.ALL, border=5)
+        self.colreg = wx.StaticText(self.main_panel, label='Regression\nequation')
+        self.mp_sizer.Add(self.colreg, pos=(0,4), flag=wx.ALL, border=5)
 
 
-        self.keyH2 = wx.StaticText(main_panel, label='H2')
-        mp_sizer.Add(self.keyH2, pos=(1,0), span= (2,1), flag=wx.ALL, border=5)
-        self.cvheavy = wx.StaticText(main_panel, label='Heavy')
-        mp_sizer.Add(self.cvheavy, pos=(1,1), flag=wx.ALL, border=5)
-        self.valheavy = wx.StaticText(main_panel, label='Body mass index > 30 kg/m**2')
-        mp_sizer.Add(self.valheavy, pos=(1,2), flag=wx.ALL, border=5)
-        self.pointheavy = wx.CheckBox(main_panel, -1, label='2')
-        mp_sizer.Add(self.pointheavy, pos=(1,3), flag=wx.ALL, border=5)
-        self.regheavy = wx.SpinCtrlDouble(main_panel, -1, value='0')
+        self.keyH2 = wx.StaticText(self.main_panel, label='H2')
+        self.mp_sizer.Add(self.keyH2, pos=(1,0), span= (2,1), flag=wx.ALL, border=5)
+        self.cvheavy = wx.StaticText(self.main_panel, label='Heavy')
+        self.mp_sizer.Add(self.cvheavy, pos=(1,1), flag=wx.ALL, border=5)
+        self.valheavy = wx.StaticText(self.main_panel, label='Body mass index > 30 kg/m**2')
+        self.mp_sizer.Add(self.valheavy, pos=(1,2), flag=wx.ALL, border=5)
+        self.pointheavy = wx.CheckBox(self.main_panel, -1, label='2')
+        self.mp_sizer.Add(self.pointheavy, pos=(1,3), flag=wx.ALL, border=5)
+        self.regheavy = wx.SpinCtrlDouble(self.main_panel, -1, value='0')
         self.regheavy.SetMinSize((60,-1))
         self.regheavy.SetDigits(2)
         self.regheavy.SetToolTip(self.regValToolTip('BMI'))
-        mp_sizer.Add(self.regheavy, pos=(1,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regheavy, pos=(1,4), flag=wx.ALL, border=5)
 
-        self.cvhtn = wx.StaticText(main_panel, label='Hypertension')
-        mp_sizer.Add(self.cvhtn, pos=(2,1), flag=wx.ALL, border=5)
-        self.valhtn = wx.StaticText(main_panel, label='2 or more antihypertensive medicines')
-        mp_sizer.Add(self.valhtn, pos=(2,2), flag=wx.ALL, border=5)
-        self.pointhtn = wx.CheckBox(main_panel, -1, label='1')
-        mp_sizer.Add(self.pointhtn, pos=(2,3), flag=wx.ALL, border=5)
-        self.reghtn = wx.StaticText(main_panel, label='NA')
+        self.cvhtn = wx.StaticText(self.main_panel, label='Hypertension')
+        self.mp_sizer.Add(self.cvhtn, pos=(2,1), flag=wx.ALL, border=5)
+        self.valhtn = wx.StaticText(self.main_panel, label='2 or more antihypertensive medicines')
+        self.mp_sizer.Add(self.valhtn, pos=(2,2), flag=wx.ALL, border=5)
+        self.pointhtn = wx.CheckBox(self.main_panel, -1, label='1')
+        self.mp_sizer.Add(self.pointhtn, pos=(2,3), flag=wx.ALL, border=5)
+        self.reghtn = wx.StaticText(self.main_panel, label='NA')
         self.reghtn.SetToolTip(self.regValToolTip('HTN'))
-        mp_sizer.Add(self.reghtn, pos=(2,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.reghtn, pos=(2,4), flag=wx.ALL, border=5)
 
-        self.keyaf = wx.StaticText(main_panel, label='F')
-        mp_sizer.Add(self.keyaf, pos=(3,0), flag=wx.ALL, border=5)
-        self.cvaf = wx.StaticText(main_panel, label='Atrial Fibrillation')
-        mp_sizer.Add(self.cvaf, pos=(3,1), flag=wx.ALL, border=5)
-        self.valaf = wx.StaticText(main_panel, label='Paroxysmal or Persistent')
-        mp_sizer.Add(self.valaf, pos=(3,2), flag=wx.ALL, border=5)
-        self.pointaf = wx.CheckBox(main_panel, -1, label='3')
-        mp_sizer.Add(self.pointaf, pos=(3,3), flag=wx.ALL, border=5)
-        self.regaf = wx.ToggleButton(main_panel, -1, label='No A. Fib')
+        self.keyaf = wx.StaticText(self.main_panel, label='F')
+        self.mp_sizer.Add(self.keyaf, pos=(3,0), flag=wx.ALL, border=5)
+        self.cvaf = wx.StaticText(self.main_panel, label='Atrial Fibrillation')
+        self.mp_sizer.Add(self.cvaf, pos=(3,1), flag=wx.ALL, border=5)
+        self.valaf = wx.StaticText(self.main_panel, label='Paroxysmal or Persistent')
+        self.mp_sizer.Add(self.valaf, pos=(3,2), flag=wx.ALL, border=5)
+        self.pointaf = wx.CheckBox(self.main_panel, -1, label='3')
+        self.mp_sizer.Add(self.pointaf, pos=(3,3), flag=wx.ALL, border=5)
+        self.regaf = wx.ToggleButton(self.main_panel, -1, label='No A. Fib')
         self.regaf.SetToolTip(self.regValToolTip('AF'))
         self.regaf.Bind(wx.EVT_TOGGLEBUTTON, self.regaf_on_toggle)
-        mp_sizer.Add(self.regaf, pos=(3,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regaf, pos=(3,4), flag=wx.ALL, border=5)
 
-        self.keyph = wx.StaticText(main_panel, label='P')
-        mp_sizer.Add(self.keyph, pos=(4,0), flag=wx.ALL, border=5)
-        self.cvph = wx.StaticText(main_panel, label='Pulmonary Hypertension')
-        mp_sizer.Add(self.cvph, pos=(4,1), flag=wx.ALL, border=5)
-        self.valph = wx.StaticText(main_panel, label='Doppler Echocardiographic\nestimated Pulmonary Artery Systolic\nPressure > 35mmHg')
-        mp_sizer.Add(self.valph, pos=(4,2), flag=wx.ALL, border=5)
-        self.pointph = wx.CheckBox(main_panel, -1, label='1')
-        mp_sizer.Add(self.pointph, pos=(4,3), flag=wx.ALL, border=5)
-        self.regph = wx.SpinCtrlDouble(main_panel, -1, value='0')
+        self.keyph = wx.StaticText(self.main_panel, label='P')
+        self.mp_sizer.Add(self.keyph, pos=(4,0), flag=wx.ALL, border=5)
+        self.cvph = wx.StaticText(self.main_panel, label='Pulmonary Hypertension')
+        self.mp_sizer.Add(self.cvph, pos=(4,1), flag=wx.ALL, border=5)
+        self.valph = wx.StaticText(self.main_panel, label='Doppler Echocardiographic\nestimated Pulmonary Artery Systolic\nPressure > 35mmHg')
+        self.mp_sizer.Add(self.valph, pos=(4,2), flag=wx.ALL, border=5)
+        self.pointph = wx.CheckBox(self.main_panel, -1, label='1')
+        self.mp_sizer.Add(self.pointph, pos=(4,3), flag=wx.ALL, border=5)
+        self.regph = wx.SpinCtrlDouble(self.main_panel, -1, value='0')
         self.regph.SetMinSize((60,-1))
         self.regph.SetDigits(2)
         self.regph.SetToolTip(self.regValToolTip('PH'))
-        mp_sizer.Add(self.regph, pos=(4,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regph, pos=(4,4), flag=wx.ALL, border=5)
 
-        self.keyold = wx.StaticText(main_panel, label='E')
-        mp_sizer.Add(self.keyold, pos=(5,0), flag=wx.ALL, border=5)
-        self.cvold = wx.StaticText(main_panel, label='Elder')
-        mp_sizer.Add(self.cvold, pos=(5,1), flag=wx.ALL, border=5)
-        self.valold = wx.StaticText(main_panel, label='Age > 60 years (years for regression)')
-        mp_sizer.Add(self.valold, pos=(5,2), flag=wx.ALL, border=5)
-        self.pointold = wx.CheckBox(main_panel, -1, label='1')
-        mp_sizer.Add(self.pointold, pos=(5,3), flag=wx.ALL, border=5)
-        self.regold = wx.SpinCtrlDouble(main_panel, -1, value='0')
+        self.keyold = wx.StaticText(self.main_panel, label='E')
+        self.mp_sizer.Add(self.keyold, pos=(5,0), flag=wx.ALL, border=5)
+        self.cvold = wx.StaticText(self.main_panel, label='Elder')
+        self.mp_sizer.Add(self.cvold, pos=(5,1), flag=wx.ALL, border=5)
+        self.valold = wx.StaticText(self.main_panel, label='Age > 60 years (years for regression)')
+        self.mp_sizer.Add(self.valold, pos=(5,2), flag=wx.ALL, border=5)
+        self.pointold = wx.CheckBox(self.main_panel, -1, label='1')
+        self.mp_sizer.Add(self.pointold, pos=(5,3), flag=wx.ALL, border=5)
+        self.regold = wx.SpinCtrlDouble(self.main_panel, -1, value='0')
         self.regold.SetMinSize((60, -1))
         self.regold.SetDigits(2)
         self.regold.SetToolTip(self.regValToolTip('Elder'))
-        mp_sizer.Add(self.regold, pos=(5,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regold, pos=(5,4), flag=wx.ALL, border=5)
 
-        self.keyf = wx.StaticText(main_panel, label='F')
-        mp_sizer.Add(self.keyf, pos=(6,0), flag=wx.ALL, border=5)
-        self.cvf = wx.StaticText(main_panel, label='Filling Pressure')
-        mp_sizer.Add(self.cvf, pos=(6,1), flag=wx.ALL, border=5)
-        self.valf = wx.StaticText(main_panel, label='Doppler Echocardiographic E/e` > 9')
-        mp_sizer.Add(self.valf, pos=(6,2), flag=wx.ALL, border=5)
-        self.pointf = wx.CheckBox(main_panel, -1, label='1')
-        mp_sizer.Add(self.pointf, pos=(6,3), flag=wx.ALL, border=5)
-        self.regf = wx.SpinCtrlDouble(main_panel, -1, value='0')
+        self.keyf = wx.StaticText(self.main_panel, label='F')
+        self.mp_sizer.Add(self.keyf, pos=(6,0), flag=wx.ALL, border=5)
+        self.cvf = wx.StaticText(self.main_panel, label='Filling Pressure')
+        self.mp_sizer.Add(self.cvf, pos=(6,1), flag=wx.ALL, border=5)
+        self.valf = wx.StaticText(self.main_panel, label='Doppler Echocardiographic E/e` > 9')
+        self.mp_sizer.Add(self.valf, pos=(6,2), flag=wx.ALL, border=5)
+        self.pointf = wx.CheckBox(self.main_panel, -1, label='1')
+        self.mp_sizer.Add(self.pointf, pos=(6,3), flag=wx.ALL, border=5)
+        self.regf = wx.SpinCtrlDouble(self.main_panel, -1, value='0')
         self.regf.SetMinSize((60, -1))
         self.regf.SetDigits(2)
         self.regf.SetToolTip(self.regValToolTip('FP'))
-        mp_sizer.Add(self.regf, pos=(6,4), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regf, pos=(6,4), flag=wx.ALL, border=5)
 
         # score total rows
-        self.pntscore = wx.StaticText(main_panel, label='Point - Score: 0 - 9')
-        mp_sizer.Add(self.pntscore, pos=(7, 2), flag=wx.ALL | wx.ALIGN_RIGHT, border=5)
-        self.pntvalue = wx.StaticText(main_panel, label='0 - 9')
-        mp_sizer.Add(self.pntvalue, pos=(7, 3), flag=wx.ALL, border=5)
+        self.pntscore = wx.StaticText(self.main_panel, label='Point - Score: 0 - 9')
+        self.mp_sizer.Add(self.pntscore, pos=(7, 2), flag=wx.ALL | wx.ALIGN_RIGHT, border=5)
+        self.pntvalue = wx.StaticText(self.main_panel, label='0 - 9')
+        self.mp_sizer.Add(self.pntvalue, pos=(7, 3), flag=wx.ALL, border=5)
 
-        self.regscore = wx.StaticText(main_panel, label='Regression - Score: 0.0 - 1.0')
-        mp_sizer.Add(self.regscore, pos=(8, 2), flag=wx.ALL | wx.ALIGN_RIGHT, border=5)
-        self.regvalue = wx.StaticText(main_panel, label='0.0 - 1.0')
-        mp_sizer.Add(self.regvalue, pos=(8, 4), flag=wx.ALL, border=5)
+        self.regscore = wx.StaticText(self.main_panel, label='Regression - Score: 0.0 - 1.0')
+        self.mp_sizer.Add(self.regscore, pos=(8, 2), flag=wx.ALL | wx.ALIGN_RIGHT, border=5)
+        self.regvalue = wx.StaticText(self.main_panel, label='0.0 - 1.0')
+        self.mp_sizer.Add(self.regvalue, pos=(8, 4), flag=wx.ALL, border=5)
 
         # the scoring nomogram
         # need to scale for inital display and w/resize events
         frame_width, frame_height = (550, 100)
         self.nomo_img = wx.Image('nihms_score_1.png', wx.BITMAP_TYPE_PNG)
         self.nomo_img = self.nomo_img.Rescale(frame_width, frame_height, quality=wx.IMAGE_QUALITY_HIGH)
-        self.nomo_bitmap = wx.StaticBitmap(main_panel, -1, wx.Bitmap(self.nomo_img))
-        mp_sizer.Add(self.nomo_bitmap, pos=(9,0), span=(1,9), flag=wx.ALL, border=5)
+        self.nomo_bitmap = wx.StaticBitmap(self.main_panel, -1, wx.Bitmap(self.nomo_img))
+        self.mp_sizer.Add(self.nomo_bitmap, pos=(9,0), span=(1,9), flag=wx.ALL, border=5)
 
         #add cal / reset buttons
-        self.pointcalc = wx.Button(main_panel, -1, "Points Calculate")
+        self.pointcalc = wx.Button(self.main_panel, -1, "Points Calculate")
         self.pointcalc.Bind(wx.EVT_BUTTON, self.calc_points)
-        mp_sizer.Add(self.pointcalc, pos=(10,1), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.pointcalc, pos=(10,1), flag=wx.ALL, border=5)
 
-        self.regcalc = wx.Button(main_panel, -1, "Regression Calculate")
+        self.regcalc = wx.Button(self.main_panel, -1, "Regression Calculate")
         self.regcalc.Bind(wx.EVT_BUTTON, self.reg_calc_score)
-        mp_sizer.Add(self.regcalc, pos=(11,1), flag=wx.ALL, border=5)
+        self.mp_sizer.Add(self.regcalc, pos=(11,1), flag=wx.ALL, border=5)
 
-        self.reset = wx.Button(main_panel, -1, "Reset")
-        mp_sizer.Add(self.reset, pos=(10,2), flag=wx.ALL|wx.ALIGN_RIGHT, border=5)
+        self.reset = wx.Button(self.main_panel, -1, "Reset")
+        self.reset.Bind(wx.EVT_BUTTON, self.on_reset)
+        self.mp_sizer.Add(self.reset, pos=(10,2), flag=wx.ALL|wx.ALIGN_RIGHT, border=5)
 
-        main_panel.SetSizer(mp_sizer)
+        self.main_panel.SetSizer(self.mp_sizer)
+        #return main_panel
 
     def regaf_on_toggle(self, event):
         if self.regaf.GetValue():
@@ -310,7 +312,7 @@ class Main_Frame(wx.Frame):
         """
         Iterate through the datarows adding up the points and calculate the score
         :param event: from Points Calculate button
-        :return: the points calculated / score
+        :return: pointscore - - the points calculated / score
         name is the Key for the dict datarows and datapoint is the dataclass H2pdata data
         """
         pointscore = 0
@@ -326,11 +328,10 @@ class Main_Frame(wx.Frame):
     def reg_calc_score(self, event):
         """
         Again iterate through the GUI via datarows, validate the user inputs and feedback / updated as needed
-        compounded by 3 different ctrls, SpinCtrlDouble (may need to change to SpinCtrlDoubleDouble
-        for now just using the ranges used by the study but will have to open up possible values (but compatible
-        with life!)
+        compounded by 3 different ctrls, SpinCtrlDouble (may need to change for now just using the ranges used by the
+        study but will have to open up possible values (but compatible with life!)
         :param event: button event
-        :return: regressionscore
+        :return: ProbHFpEF
         The regression equation from the 2018 article; a grip for me is that none of data point has more then
         3 significant digits while equation circa 18 digits?  See the excel/calc spreadsheet
 
@@ -369,16 +370,32 @@ class Main_Frame(wx.Frame):
                 elif isinstance(ctrl, wx.StaticText):
                     datapoint.userreg = ctrl.GetLabel()
                     datapoint.userreg = 0
-            print(f'{datapoint.regval}  {datapoint.userreg}')
+            #print(f'{datapoint.regval}  {datapoint.userreg}')
             # add the product of each of these to get the final Log Odds value (F2 in comment above)
             logOdds += float(datapoint.userreg) * datapoint.regval
-            print(f'{datapoint.userreg:.2f}')
-        print(f'log Odds: {logOdds:.3f}')
+            #print(f'{datapoint.userreg:.2f}')
+        #print(f'log Odds: {logOdds:.3f}')
         G2 = 2.71828182845904**logOdds
-        print(f'G2: {G2:.3f}')
+        #print(f'G2: {G2:.3f}')
         ProbHFpEF = G2 / (1 + G2) * 100
-        print(f'ProbHFpEF: {ProbHFpEF:.3f}')
+        #print(f'ProbHFpEF: {ProbHFpEF:.3f}')
         self.regvalue.SetLabel(f'{ProbHFpEF:.3f}')
+        return ProbHFpEF
+
+    def on_reset(self, event):
+        # reset all the control
+        for key, datapoint in self.datarows.items():
+            ctrl = getattr(self, datapoint.regname, None)
+            if ctrl:
+                if isinstance(ctrl, wx.SpinCtrlDouble):
+                    ctrl.SetValue(0.0)
+                elif isinstance(ctrl, wx.ToggleButton):
+                    ctrl.SetValue(False)
+                    ctrl.SetLabel("No A. Fib")
+                elif isinstance(ctrl, wx.CheckBox):
+                    ctrl.SetValue(True)
+        #self.main_panel.Layout()
+        #self.main_panel.Refresh()
 
 
 
